4ccb68a3a6ee748a9f2c9e48048d540c69c4b009
gosto <gostohov@gmail.com>
2021-10-14 15:04:16 +0300

    feat: Добавлены компоненты для Авторизации и Регистрации, и роуты к ним

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: 'chat',
-        loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule)
-    }
+    {path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule)},
+    {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+    {path: '', redirectTo: 'chat', pathMatch: 'full'},
+    {path: '**', redirectTo: 'chat'}
 ];
 
 @NgModule({
